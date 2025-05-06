e6a781e051267a42fc808b88955449bec98e4cbe
deepa-124 <deepashankargodpatil@mail.com>
2025-05-06 15:07:20 +0530

    add new file to repo

--- Employee.java
+++ Employee.java
@@ -2,18 +2,21 @@ public class Employee {
         String name;
         int id;
         double salary;
-        public Employee(String name,int id,double salary){
+        String city;
+        public Employee(String name,int id,double salary,String city){
             this.name=name;
             this.id=id;
             this.salary=salary;
+            this.city=city;
         }
         public void displayInfo(){
             System.out.println("Employee name"+name);
             System.out.println("Employee id"+id);
             System.out.println("Employee salary"+salary);
+            System.out.println("Employee City"+city);
         }
         public static void main(String[]args){
-            Employee emp1=new Employee("xyz",101,23444.0);
+            Employee emp1=new Employee("xyz",101,23444.0,"Dharwad");
             emp1.displayInfo();
         }
     }
